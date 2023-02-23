06b15d6a9e3136172fda1ddffd757f1be6378119
MONIDEPA GHOSH <123240415+monidepag@users.noreply.github.com>
2023-02-23 14:36:31 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+
+/* 
+
+    @author: Arijit Saha
+	@desc: js for index.html (Header & Footer Template)
+	@file path from root: Ex ./index.js/
+
+*/
+
+var nrifintech="NRI TRAVEL";
+var booking = `My Bookings`; 
+var logout="Logout";
+
+// document.getElementById("nrifintech").innerHTML=nrifintech;
+// document.getElementById("booking").innerHTML = booking; 
+// document.getElementById("logout").innerHTML = logout;
+
+document.querySelector(".nrifintech").innerHTML=nrifintech;
+document.querySelector(".booking").innerHTML=booking;
+document.querySelector(".logout").innerHTML=logout;
+
+
